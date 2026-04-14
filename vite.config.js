2774fc9387aa3c36fc7bd65b4ed470786abd6134
nicos92 <nicolassandoval140692@gmail.com>
2026-04-14 07:38:45 -0300

    cambio de url sin el nombre d emi usuario

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   // Opciones de configuración personalizadas
-  base: 'https://nicos92.github.io/primer-vite-vanilla',
+  base: '/primer-vite-vanilla/',
 })
